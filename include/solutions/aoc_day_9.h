f37c83605964ed55a0b9bda96c54371c79302e10
bcooperstl <coop999@gmail.com>
2024-12-22 14:45:19 -0600

    Added day 9 part 2 solution that works for test case

--- include/solutions/aoc_day_9.h
+++ include/solutions/aoc_day_9.h
@@ -4,6 +4,8 @@
 #include "aoc_day.h"
 
 #define DAY9_MAX_BLOCKS 200000
+#define DAY9_MAX_FREE_SPACES 10000
+#define DAY9_MAX_FILES 10000
 
 namespace Day9
 {
@@ -12,11 +14,27 @@ namespace Day9
         int file_id;
     };
     
+    struct FreeSpace
+    {
+        int starting_block;
+        int num_blocks;
+    };
+    
+    struct File
+    {
+        int starting_block;
+        int num_blocks;
+    };
+    
     class DiskMap
     {
         private:
             Block m_disk_map[DAY9_MAX_BLOCKS];
+            FreeSpace m_free_spaces[DAY9_MAX_FREE_SPACES];
+            File m_files[DAY9_MAX_FREE_SPACES];
             int m_num_blocks_used;
+            int m_free_spaces_used;
+            int m_files_used;
             int m_next_file_id;
             int m_file_blocks;
         public:
@@ -25,6 +43,7 @@ namespace Day9
             void load_disk_map(string input);
             void display_disk_map();
             void remap_over_free_blocks();
+            void remap_file_based();
             long compute_checksum();
     };
 }
--- src/solutions/aoc_day_9.cpp
+++ src/solutions/aoc_day_9.cpp
@@ -20,6 +20,8 @@ namespace Day9
         m_num_blocks_used = 0;
         m_next_file_id=0;
         m_file_blocks=0;
+        m_free_spaces_used=0;
+        m_files_used=0;
     }
     
     DiskMap::~DiskMap()
@@ -39,6 +41,9 @@ namespace Day9
                 cout << "Loading file id " << m_next_file_id << " taking up " << count << " blocks from positions " 
                      << m_num_blocks_used << " to " << m_num_blocks_used + count - 1 << endl;
 #endif
+                m_files[m_files_used].starting_block=m_num_blocks_used;
+                m_files[m_files_used].num_blocks=count;
+                m_files_used++;
                 for (int block=m_num_blocks_used; block < m_num_blocks_used + count; block++)
                 {
                     m_disk_map[block].file_id=m_next_file_id;
@@ -54,6 +59,9 @@ namespace Day9
                 cout << "Loading free space taking up " << count << " blocks from positions " 
                      << m_num_blocks_used << " to " << m_num_blocks_used + count - 1 << endl;
 #endif
+                m_free_spaces[m_free_spaces_used].starting_block=m_num_blocks_used;
+                m_free_spaces[m_free_spaces_used].num_blocks=count;
+                m_free_spaces_used++;
                 for (int block=m_num_blocks_used; block < m_num_blocks_used + count; block++)
                 {
                     m_disk_map[block].file_id=DAY_9_FREE_SPACE;
@@ -64,6 +72,7 @@ namespace Day9
         }
 #ifdef DEBUG_DAY_9
         cout << "Loaded " << m_num_blocks_used << " blocks containing " << m_next_file_id << " files using " << m_file_blocks << " blocks." << endl;
+        cout << "Loaded " << m_free_spaces_used << " free space sections" << endl;
 #endif
         return;
     }
@@ -119,6 +128,70 @@ namespace Day9
 #endif
             }
             file_pos--;
+        }
+    }
+    
+    void DiskMap::remap_file_based()
+    {
+        int file_idx = m_files_used-1;
+        while (file_idx > 0)
+        {
+            int free_idx=0;
+            
+            int target_free_idx=0;
+            bool free_location_found=false;
+            
+            while (m_free_spaces[free_idx].starting_block < m_files[file_idx].starting_block)
+            {
+                if (m_free_spaces[free_idx].num_blocks >= m_files[file_idx].num_blocks)
+                {
+                    target_free_idx=free_idx;
+                    free_location_found = true;
+                    break;
+                }
+                free_idx++;
+            }
+
+            if (free_location_found == true)
+            {
+#ifdef DEBUG_DAY_9
+                cout << "Moving file " << file_idx 
+                     << " from blocks " << m_files[file_idx].starting_block
+                     << " to " << m_files[file_idx].starting_block + m_files[file_idx].num_blocks - 1
+                     << " to empty space at blocks " << m_free_spaces[target_free_idx].starting_block
+                     << " to " << m_free_spaces[target_free_idx].starting_block + m_files[file_idx].num_blocks - 1 << endl;
+#endif
+                for (int i=0; i<m_files[file_idx].num_blocks; i++)
+                {
+                    m_disk_map[m_free_spaces[target_free_idx].starting_block+i].file_id = file_idx;
+                    m_disk_map[m_files[file_idx].starting_block+i].file_id = DAY_9_FREE_SPACE;
+                }
+                m_files[file_idx].starting_block=m_free_spaces[target_free_idx].starting_block;
+
+#ifdef DEBUG_DAY_9
+                cout << "Modified free space of " << m_free_spaces[target_free_idx].num_blocks
+                     << " blocks at " << m_free_spaces[target_free_idx].starting_block;
+#endif
+
+                m_free_spaces[target_free_idx].starting_block+=m_files[file_idx].num_blocks;
+                m_free_spaces[target_free_idx].num_blocks-=m_files[file_idx].num_blocks;
+
+#ifdef DEBUG_DAY_9
+                cout << " to be " << m_free_spaces[target_free_idx].num_blocks
+                     << " blocks at " << m_free_spaces[target_free_idx].starting_block << endl;
+#endif
+#ifdef DEBUG_DAY_9_SWAPS
+                display_disk_map();
+#endif
+            }
+            else
+            {
+#ifdef DEBUG_DAY_9
+                cout << "No free space to move file " << file_idx << " which is " << m_files[file_idx].num_blocks << " blocks" << endl;
+#endif
+            }
+            
+            file_idx--;
         }
     }
     
@@ -184,7 +257,22 @@ string AocDay9::part2(string filename, vector<string> extra_args)
 {
     string data = read_input(filename);
 
+    DiskMap diskmap;
+    
+    diskmap.load_disk_map(data);
+#ifdef DEBUG_DAY_9
+    cout << "Starting map:" << endl;
+    diskmap.display_disk_map();
+#endif
+
+    diskmap.remap_file_based();
+
+#ifdef DEBUG_DAY_9
+    cout << "Ending map:" << endl;
+    diskmap.display_disk_map();
+#endif
+    
     ostringstream out;
-    out << "Day 9 - Part 2 not implemented";
+    out << diskmap.compute_checksum();
     return out.str();
 }
